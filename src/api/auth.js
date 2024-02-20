0ef1cdf8f658b68a03a076fcc0f74ef0220ebfaf
Nate R <natemreinhardt@gmail.com>
2024-02-20 00:07:35 -0600

    refactor

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,21 +1,22 @@
 const crypto = require('crypto')
 
 const clientId = '308a87f731b343f7bc74f72d0186ff1d'
-const codeVerifier = crypto.randomBytes(32).toString('base64')
-console.log('Code Verifier', codeVerifier)
-const generateCodeChallenge = () => {
-  const hash = crypto.createHash('sha256').update(codeVerifier).digest()
-  const base64Url = hash
+
+const buildAuthUrl = () => {
+  const codeVerifier = crypto
+    .randomBytes(32)
     .toString('base64')
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=+$/, '')
-  return base64Url
-}
+  const codeChallenge = crypto
+    .createHash('sha256')
+    .update(codeVerifier)
+    .digest('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '')
 
-const codeChallenge = generateCodeChallenge()
-
-const buildAuthUrl = () => {
   const url = new URL('https://login.eveonline.com/v2/oauth/authorize/')
   const params = new URLSearchParams({
     response_type: 'code',
@@ -27,33 +28,25 @@ const buildAuthUrl = () => {
     state: 'yolo'
   })
   url.search = params.toString()
-  return url.toString()
+  return { url: url.toString(), codeVerifier: codeVerifier }
 }
 
-const getAccessToken = async (code) => {
-  console.log('Getting Access Token', code)
+const getAccessToken = async (code, codeVerifier) => {
+  console.log('Getting Access Token', code, codeVerifier)
 
   const url = new URL('https://login.eveonline.com/v2/oauth/token')
 
-  const body = {
-    data: {
-      grant_type: 'authorization_code',
-      clientId: clientId,
-      code: code,
-      code_verifier: codeVerifier
-    }
-  }
-
-  console.log('Body', body)
+  const formValues = `grant_type=authorization_code&client_id=${clientId}&code=${code}&code_verifier=${codeVerifier}`
+  console.log('Form Values', formValues)
   const requestOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       Host: 'login.eveonline.com'
     },
-    body: JSON.stringify(body)
+    body: formValues
   }
-
+  console.log(requestOptions)
   try {
     const response = await fetch(url, requestOptions)
     const data = await response.json()
